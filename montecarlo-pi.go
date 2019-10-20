39837425e303294b2ceca5673f0b7034a4d4799b
David Ison <developer@dtison.net>
2019-10-20 14:12:11 -0400

    Small changes.

--- montecarlo-pi.go
+++ montecarlo-pi.go
@@ -7,33 +7,25 @@ import (
 	"time"
 )
 
-/* type count64 int64
+/*
+const MaxUint = ^uint(0)
+const MinUint = 0
+const MaxInt = int(MaxUint >> 1)
+const MinInt = -MaxInt - 1
 
-func (c *count64) increment() int64 {
-	var next int64
-	for {
-		next = int64(*c) + 1
-		if atomic.CompareAndSwapInt64((*int64)(c), int64(*c), next) {
-			return next
-		}
-	}
-}
-func (c *count64) get() int64 {
-	return atomic.LoadInt64((*int64)(c))
+
 } */
 
 func main() {
 	fmt.Println("GOMAXPROCS set from", runtime.GOMAXPROCS(runtime.NumCPU()), "to", runtime.GOMAXPROCS(0))
 	rand.Seed(time.Now().UnixNano())
-	monteCarloPi(5000000000)
+	monteCarloPi(1000000000)
 }
 
 func monteCarloPi(samples int) {
 
 	numCPUs := runtime.NumCPU()
-
 	samplesPerThread := samples / numCPUs
-
 	threadResults := make(chan uint64, numCPUs)
 
 	for i := 0; i < numCPUs; i++ {
@@ -52,23 +44,12 @@ func monteCarloPi(samples int) {
 			threadResults <- pointsInside
 		}()
 
-		// x, y := rand.Float64(), rand.Float64()
-		// if (x*x)+(y*y) <= 1.0 {
-		// 	pointsInside++
-		// }
-
 	}
-	//	fmt.Println(4.0 * float64(<-points2) / float64(n))
 
 	var total uint64
-	// for result := range threadResults {
-	// 	total += result
-	// }
-
 	for i := 0; i < numCPUs; i++ {
 		total += <-threadResults
 	}
-	//total *= 4.0
 
 	fmt.Printf("%f\n", float64(total)/float64(samples)*4.0)
 
